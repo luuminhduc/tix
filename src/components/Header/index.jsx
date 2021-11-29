41101dd2e3938f27f09f5dbcef6a30413d2956da
Duc Luu <luuminhduc00@gmail.com>
2021-11-29 17:35:31 +0700

    solve conflict in index.html

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/images/web-logo.png";
 const Header = () => {
+  const [showNav, setShowNav] = useState(false);
   return (
     <div className="header">
       <Link to="/">
         <img src={Logo} alt="" />
       </Link>
-      <nav>
+      <span
+        onClick={() => setShowNav(true)}
+        class="material-icons nav_open_btn"
+      >
+        menu
+      </span>
+      <nav className={`${showNav && "active"}`}>
+        <span
+          onClick={() => setShowNav(false)}
+          class="material-icons nav_close_btn"
+        >
+          close
+        </span>
+
         <div className="nav_link_list">
           <Link to="/">Lich chieu</Link>
           <Link to="/">Lich chieu</Link>
